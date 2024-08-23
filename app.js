5512eec156b6378d78d238daa0e141bc4b6d72d0
wahab <wahabrasool08@gmail.com>
2024-08-23 20:06:49 +0500

    Ticker Clock

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const template = document.querySelector("#clocknumbers");
+
+document.querySelectorAll('ul').forEach((n, i)=>{
+  const clone = template.content.cloneNode(true);
+  n.appendChild(clone);
+})
+
+const update = function(delta) {
+  const d = new Date();
+  document.body.style.setProperty('--seconds-p', Math.floor(d.getSeconds() / 10));
+  document.body.style.setProperty('--seconds-a', d.getSeconds() % 10);
+
+  document.body.style.setProperty('--minutes-p', Math.floor(d.getMinutes() / 10));
+  document.body.style.setProperty('--minutes-a', d.getMinutes() % 10);
+
+  document.body.style.setProperty('--hours-p', Math.floor(d.getHours() / 10));
+  document.body.style.setProperty('--hours-a', d.getHours() % 10);
+
+  window.requestAnimationFrame(update);
+}
+window.requestAnimationFrame(update);
